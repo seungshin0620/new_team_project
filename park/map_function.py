d2521354eafcd14eefec790f4a16cd60a7c039ef
ParkHoHyeon0921 <ghgusghgus2085@gmail.com>
2023-07-05 21:40:38 +0900

    comittest file

--- park/map_function.py
+++ park/map_function.py
@@ -10,18 +10,27 @@ class MapFunction(QWidget, mapf):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setMaximumSize(100, 100)
-        self.initself()
-    def initself(self):
+        self.resize(1000, 1000)
         self.a = mpampa()
-        self.a.real_map(37, 127)
-        self.a.show()
+        # self.a.real_map()
+        self.label_1 = QWidget(self)
+        self.laout_ = QVBoxLayout(self.label_1)
+        self.laout_.addWidget(self.a)
+        self.label_1.resize(1000, 1000)
+        self.label_1.show()
+        # self.vlaout = QVBoxLayout(self)
+        # self.vlaout.addWidget(self.a)
+        # widget = QWidget(self)
+        # widget.setLayout(self.vlaout)
+        # self.a.show()
 
 
+    # def initself(self):
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mapwidget = MapFunction()
+    mapwidget.show()
     sys.exit(app.exec_())
 
--- park/tetest.py
+++ park/tetest.py
@@ -9,13 +9,25 @@ from PyQt5.QtWidgets import QWidget, QApplication
 class mpampa(QWidget):
     def __init__(self):
         super().__init__()
-
-    def real_map(self, Latitud, longitude):
-        m = folium.Map(location=[Latitud, longitude], zoom_start=16)
+        # self.resize(500, 500)
+        """
+        self.latitude = None
+        self.longitude = None
+        m = folium.Map(location=[latitude, longitude], zoom_start=16)
+        """
+        m = folium.Map(location=[37, 127], zoom_start=16)
         w = QtWebEngineWidgets.QWebEngineView(self)
         w.setHtml(m.get_root().render())
-        w.setMinimumSize(100, 100)
+        w.setMinimumSize(1000, 1000)
+        w.setMaximumSize(16447, 16447)
         w.show()
+
+    """
+    def set_location(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+    """
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
